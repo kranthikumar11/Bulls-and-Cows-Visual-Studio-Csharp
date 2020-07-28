1334ecd5bdbb98e8325516959af45f588df15e98
kranthikumar11 <40728061+kranthikumar11@users.noreply.github.com>
2020-07-28 17:35:24 +0530

    Add files via upload

---
+++ Bulls_And_Cows/Bulls_And_Cows/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace Bulls_And_Cows
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gameInstructionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripLabel3 = new System.Windows.Forms.ToolStripLabel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(327, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gameInstructionsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // gameInstructionsToolStripMenuItem
+            // 
+            this.gameInstructionsToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("gameInstructionsToolStripMenuItem.Image")));
+            this.gameInstructionsToolStripMenuItem.Name = "gameInstructionsToolStripMenuItem";
+            this.gameInstructionsToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.I)));
+            this.gameInstructionsToolStripMenuItem.Size = new System.Drawing.Size(207, 22);
+            this.gameInstructionsToolStripMenuItem.Text = "Game Instructions";
+            this.gameInstructionsToolStripMenuItem.Click += new System.EventHandler(this.gameInstructionsToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("exitToolStripMenuItem.Image")));
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(207, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(15, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 80);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "New Game";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Secret Number : ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(105, 116);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PasswordChar = '*';
+            this.textBox1.Size = new System.Drawing.Size(127, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(247, 114);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Give Up";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 153);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Your Guess : ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(105, 146);
+            this.textBox2.MaxLength = 4;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(127, 20);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(247, 144);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Go";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoScroll = true;
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 103F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(20, 185);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 39F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 61F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(307, 100);
+            this.tableLayoutPanel1.TabIndex = 8;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.toolStripLabel2,
+            this.toolStripLabel3});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 301);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(327, 25);
+            this.toolStrip1.TabIndex = 9;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(0, 22);
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(0, 22);
+            // 
+            // toolStripLabel3
+            // 
+            this.toolStripLabel3.Name = "toolStripLabel3";
+            this.toolStripLabel3.Size = new System.Drawing.Size(0, 22);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(102, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(327, 326);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "4 Digits";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gameInstructionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel3;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Bulls_And_Cows/Bulls_And_Cows/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bulls_And_Cows
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static int secret,bulls=0,cows=0,attempts=0;
+        static  Stopwatch stopWatch = new Stopwatch();
+
+        private void gameInstructionsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("The Computer generates a 4 - digit Random secret number. The digits are all different. Then,the player tries to guess the number and Computer gives the number of matches. If the matching digits are in their right positions, they are 'bulls', if in different positions, they are 'cows'.");
+          }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            stopWatch.Reset();
+            stopWatch.Start();
+            Random r = new Random();
+            int d1 = r.Next(1, 10), d2 = r.Next(1, 10), d3 = r.Next(1, 10), d4 = r.Next(1, 10);
+            while (d1 == d2 || d1 == d3 || d1 == d4 || d2 == d3 || d2 == d4 || d3 == d4)
+            {
+                if (d1 == d2 || d2 == d3 || d2 == d4)
+                {
+                    d2 = r.Next(1, 10);
+                }
+                if (d1 == d3 || d2 == d3 || d3 == d4)
+                {
+                    d3 = r.Next(1, 10);
+                }
+                if (d1 == d4 || d2 == d4 || d3 == d4)
+                {
+                    d4 = r.Next(1, 10);
+                }
+            }
+            secret = d1 * 1000 + d2 * 100 + d3 * 10 + d4 * 1;
+            textBox1.Text = secret.ToString();
+            textBox2.Enabled = true;
+            button2.Enabled = true;
+            tableLayoutPanel1.Controls.Clear();
+            tableLayoutPanel1.RowStyles.Clear();
+            Label l1 = new Label();
+            Label l2 = new Label();
+            Label l3 = new Label();
+            l1.Text = "Attempt No. ";
+            tableLayoutPanel1.Controls.Add(l1);
+            l2.Text = "Guess ";
+            tableLayoutPanel1.Controls.Add(l2);
+            l3.Text = "Result ";
+            tableLayoutPanel1.Controls.Add(l3);
+            toolStripLabel1.Text = "";
+            toolStripLabel2.Text = "";
+            toolStripLabel3.Text = "";
+            bulls = 0;
+            cows = 0;
+            attempts = 0;
+            label3.Text = "";
+            textBox2.Text = "";
+            textBox2.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            toolStripLabel1.Text = "Status : You Lost";
+            toolStripLabel2.Text = "Attempts : "+attempts.ToString();
+            toolStripLabel3.Text = "Secret no. is : " + secret.ToString();
+            textBox2.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            label3.Text = "";
+            textBox2.Text = "";
+            stopWatch.Reset();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            foreach (char c in textBox2.Text.ToCharArray()) 
+            { 
+                if (System.Text.RegularExpressions.Regex.IsMatch(c.ToString(), "[^0-9]"))
+                {
+                    List<char> l = textBox2.Text.ToCharArray().ToList();
+                    l.Remove(c);
+                    textBox2.Text = new string(l.ToArray());
+                    textBox2.SelectionStart = textBox1.Text.Length;
+                    textBox1.SelectionLength = 0; 
+                }
+            }
+            if (textBox2.Text.Length == 4)
+            {
+                if (System.Text.RegularExpressions.Regex.IsMatch(textBox2.Text, "^(?!.*(.).*\\1)\\d{4}"))
+                {
+                    label3.Text = "";
+                    button3.Enabled = true;
+
+                }
+                else
+                {
+                    label3.Text="Enter 4-digit number without repetition";
+                    textBox2.Text = "";
+                }
+            }
+            else
+                button3.Enabled = false;      
+        }
+
+        public void count_bulls(int a,int b)
+        {
+            if (a == b)
+                bulls++;
+        }
+        public void count_cows(int a,int b,int c,int d)
+        {
+            if (a == b)
+                cows++;
+            else if (a == c)
+                cows++;
+            else if (a == d)
+                cows++;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            bulls = 0;
+            cows = 0;
+            int number = int.Parse(textBox2.Text);
+            attempts++;
+            Label l1 = new Label();
+            Label l2 = new Label();
+            Label l3 = new Label();
+            if (secret==number)
+            {
+                stopWatch.Stop();
+                bulls = 4;
+                cows = 0;
+                l1.Text = attempts.ToString();
+                l2.Text = number.ToString();
+                l3.Text = bulls + " Bulls and " + cows + " Cows";
+                tableLayoutPanel1.Controls.Add(l1);
+                tableLayoutPanel1.Controls.Add(l2);
+                tableLayoutPanel1.Controls.Add(l3);
+                toolStripLabel1.Text = "Status : You Won";
+                toolStripLabel2.Text = "Attempts : "+attempts.ToString();
+                TimeSpan ts = stopWatch.Elapsed;
+                string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}",ts.Hours, ts.Minutes, ts.Seconds);
+                toolStripLabel3.Text = "Time taken : "+ elapsedTime;
+                textBox2.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+
+            }
+            else
+            {
+                int snum1 = (secret / 1000) % 10;
+                int snum2=(secret / 100) % 10;
+                int snum3= (secret / 10) % 10;
+                int snum4= (secret / 1) % 10;
+                int num1 = (number / 1000) % 10;
+                int num2= (number / 100) % 10;
+                int num3= (number / 10) % 10;
+                int num4= (number / 1) % 10;
+                count_bulls(snum1, num1);
+                count_bulls(snum2, num2);
+                count_bulls(snum3, num3);
+                count_bulls(snum4, num4);
+                count_cows(num1, snum2, snum3, snum4);
+                count_cows(num2, snum1, snum3, snum4);
+                count_cows(num3, snum1, snum2, snum4);
+                count_cows(num4, snum1, snum2, snum3);
+                l1.Text = attempts.ToString();
+                l2.Text = number.ToString();
+                l3.Text = bulls + " Bulls and " + cows + " Cows";
+                tableLayoutPanel1.Controls.Add(l1);
+                tableLayoutPanel1.Controls.Add(l2);
+                tableLayoutPanel1.Controls.Add(l3);
+            }
+            textBox2.Text = "";
+            textBox2.Focus();
+            button3.Enabled = false;
+        }
+        
+    }
+}
